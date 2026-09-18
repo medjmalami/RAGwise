40dc117a2164ab4e9a511888def04c22b549ee17
medjmalami <jmalmohamedamine1@gmail.com>
2026-09-18 18:19:55 +0100

    feat: add env variables for langfuse

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
     gemini_api_key: str
+    langfuse_public_key: str
+    langfuse_secret_key: str
+    langfuse_base_url: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
