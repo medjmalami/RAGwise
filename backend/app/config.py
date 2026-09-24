18725fc06c8c3fdbb6f24641f5b8c2c4f043d40d
medjmalami <jmalmohamedamine1@gmail.com>
2026-09-24 16:33:55 +0100

    feat: adding cohere model name as env variable

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     langfuse_secret_key: str
     langfuse_base_url: str
     cohere_api_key: str
+    cohere_rerank_model: str = "rerank-v3.5"
 
     model_config = SettingsConfigDict(env_file=".env")
 
